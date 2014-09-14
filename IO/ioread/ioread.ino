977725a834068dc77854033476346ef91f9d9976
Fujun Wu <fwu@altheadx.com>
2014-09-13 18:36:56 -0700

    Direct IO read to improve the speed

--- IO/ioread/ioread.ino
+++ IO/ioread/ioread.ino
@@ -1,10 +1,22 @@
 
-bool start=1;
+#include <avr/pgmspace.h>
+#include <pins_arduino.h>
 
 #define pin_PWM 13
 #define pin_read 2
 
+//volatile uint8_t *P_RS, *P_WR, *P_RD, *P_CS;
+//uint8_t B_RS, B_WR, B_RD, B_CS;
+
+volatile uint8_t *port_read;
+uint8_t bit_read;
+
 void setup() {
+//  P_RS	= portOutputRegister(digitalPinToPort(pin_read));
+//  B_RS	= digitalPinToBitMask(pin_read);
+
+  port_read = portInputRegister(digitalPinToPort(pin_read));
+  bit_read = digitalPinToBitMask(pin_read);
   pinMode(pin_PWM, OUTPUT);
   pinMode(pin_read, INPUT);
   analogWrite(pin_PWM,10);
@@ -22,7 +34,8 @@ void loop() {
   unsigned long pcount=0;
   unsigned long ncount=0;
   while(timer0_millis < stop_time) {
-    int d=digitalRead(pin_read);
+    //int d=digitalRead(pin_read);
+    uint8_t d = (*port_read)& bit_read;
     if (d==0)
         ncount++;
       else
