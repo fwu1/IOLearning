44ce5363d2d1470d018603b34070e61fc3bc40c0
Fujun Wu <fwu@altheadx.com>
2014-09-13 17:20:35 -0700

    Init

---
+++ pwm/analogWrite/analogWrite.ino
@@ -0,0 +1,11 @@
+void setup() {
+  pinMode(13, OUTPUT);
+  pinMode(11, OUTPUT);
+  analogWrite(11,50);
+  analogWrite(13,128);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
---
+++ pwm/fastPWM/fastPWM.ino
@@ -0,0 +1,21 @@
+void setup() {
+  pinMode(3, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(13, OUTPUT);
+/*  
+  TCCR2A = _BV(COM2A1) | _BV(COM2B1) | _BV(WGM21) | _BV(WGM20);
+  TCCR2B = _BV(CS22);
+  OCR2A = 180;
+  OCR2B = 50;
+ */
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(13, HIGH);
+  digitalWrite(11, HIGH);
+  delayMicroseconds(500); // Approximately 10% duty cycle @ 1KHz
+  digitalWrite(13, LOW);
+  digitalWrite(11, LOW);
+  delayMicroseconds(1000 - 500);
+}
